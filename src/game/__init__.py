692a4f78382bd33da5811fd81246607a07bab3e1
arpitchakladar <54011232+arpitchakladar@users.noreply.github.com>
2022-10-03 07:37:56 +0530

    Display game board

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -4,20 +4,36 @@ import pygame
 class Game:
     is_running: bool
     window: pygame.Surface
+    display_size: int
 
-    def __init__(self):
-        run = False
-        window = pygame.display.set_mode((640, 640))
+    def __init__(self, display_size: int = 640):
+        self.is_running = False
+        self.window = pygame.display.set_mode((display_size, display_size))
+        self.display_size = display_size
+
+    def draw_board(self):
+        cell_size = self.display_size / 8
+        dark_color = (125, 206, 160)
+        light_color = (232, 248, 245)
+        for i in range(8):
+            for j in range(8):
+                color = dark_color
+                if (i + j) % 2 == 0:
+                    color = light_color
+                pygame.draw.rect(self.window, color, pygame.Rect(i * cell_size, j * cell_size, cell_size, cell_size))
 
     def run(self) -> NoReturn:
-        is_running = True
+        self.is_running = True
+        pygame.init()
         pygame.display.set_caption("Zess")
         icon = pygame.image.load("resources/images/zess-logo.png")
         pygame.display.set_icon(icon)
-        while is_running:
+        while self.is_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    is_running = False
+                    self.is_running = False
                     break
+                self.draw_board()
+                pygame.display.update()
 
         pygame.quit()
